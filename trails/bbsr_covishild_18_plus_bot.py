77aedcf0597f8fde1aa3b4057fb9f312763259b4
amlan <amlana.x.dwivedy@kp.org>
2021-06-06 00:28:36 +0530

    telegram message updated to cleaner

--- trails/bbsr_covishild_18_plus_bot.py
+++ trails/bbsr_covishild_18_plus_bot.py
@@ -6,7 +6,7 @@ import time
 BASE_COWIN_URL = "https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByDistrict"
 now = datetime.now()
 today_date = now.strftime("%d-%m-%Y")
-odisha_khurda_cuttack_angul_dkl_ids = [471]
+odisha_khurda_cuttack_angul_dkl_ids = [446]
 is_for_eighteen_plus = False
 is_for_second_dosage = True
 telegram_api_url = "https://api.telegram.org/bot1832543686:AAFgdgcpIOkNeIBWe07jxwVG5uNW1FJX5N4/sendMessage?chat_id=@__group_id__&parse_mode=HTML&text="
@@ -60,46 +60,58 @@ def extract_availability_data(response):
     # print(message)
     global last_message
     if last_message != message:
-        print("Last message is not equal to message")
+        print("Last message is not equal to message {} at {}".format(last_message, datetime.now().strftime("%H:%M")))
         last_message = message
     else:
-        print("Last message is  equal to message")
+        print("Last message is  equal to message at {}".format(datetime.now().strftime("%H:%M")))
         return
+    # print(message)
     if len(message) > 0:
-        # message += "\nYou can join the Odisha Covishield 18+ channel https://t.me/odisha_covishild_18_plus. And for feedback use this group https://t.me/OdishaVaccineFeedback. Share it with more people".format(
-        #     now.strftime("%H:%m"))
         send_telegram_message(message)
     else:
         print("No Slots available at {}".format(datetime.now().strftime("%H:%M")))
-    #     send_telegram_message(
-    #         "No slots available now. Last checked at {}. You can join the Odisha Covaxine 18+ channel https://t.me/odisha_vovaxine_18_plus".format(
-    #             datetime.now().strftime("%H:%m")))
 
 
 def build_message(center, session):
-    return "📍{} ,{} , {}📍" \
-           "\n<strong>Age:{}</strong>" \
-           "\n💉💉💉<b>{}</b>" \
-           ",<code>{}</code>" \
-           "\n📅<b><u>{}</u></b>" \
-           "\n<strong>Quantity {}<code>[D1:{},D2:{}]</code></strong>" \
+    vaccine_fee = ""
+    try:
+        fees = center["vaccine_fees"]
+        for fee in fees:
+            vaccine_fee += fee["vaccine"] + ": ₹" + fee["fee"]
+    except  Exception as e:
+        print(e)
+
+    date_time_string = session["date"]
+    date_time_obj = datetime.strptime(date_time_string, "%d-%m-%Y")
+    date_text = date_time_obj.strftime("%d %b, %Y")
+    text = "District+: <b>{}</b>\n" \
+           "Age:<b>{}</b>\n" \
+           "Pincode:<b>{}</b>\n" \
+           "📍 <b><i>{}</i></b> 📍" \
+           "\nDate: <b>{}</b>" \
+           "\n💉Vaccine: <code><b>{}</b></code>" \
+           "\nFee: <b>{} " \
+           "\n{}</b>" \
+           "\n<strong>Total {} Slots <code>[1st Dosage:{},2nd Dosage:{}]</code></strong>" \
            "\n\n" \
-           " %0A" \
-        .format(center["name"]
-                , center["district_name"]
-                , center["pincode"]
-                ,str(session["min_age_limit"])+"+",
+        .format(center["district_name"],
+                session["min_age_limit"],
+                center["pincode"],
+                center["name"] + ", " + center["address"],
+                date_text,
                 session["vaccine"],
                 center["fee_type"],
-                session["date"],
+                vaccine_fee,
                 session["available_capacity"],
-                session[
-                    "available_capacity_dose1"],
-                session[
-                    "available_capacity_dose2"])
+                session["available_capacity_dose1"],
+                session["available_capacity_dose2"])
+    print(text)
+    return text
 
 
 def send_telegram_message(message):
+    # if True:
+    #     return
     final_telegram_url = telegram_api_url.replace("__group_id__", group_id_forty_five)
     final_telegram_url_with_message = final_telegram_url + message
     response = requests.get(final_telegram_url_with_message)
